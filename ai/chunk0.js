8c57ef183493f824b6b03dfa7553dd86ed00546c
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-08-27 07:11:20 -0700

    Update chunk0.js

--- ai/chunk0.js
+++ ai/chunk0.js
@@ -10,34 +10,45 @@ const setup = (function(inst = {
 	function ranItem(stringOrArray) {
 		return stringOrArray[Math.floor(Math.random() * stringOrArray.length)]
 	}
+	const reheirbase = {
+		hello: [
+			[" "],
+			["Well h", "H"],
+			["ello", "ey", "i"],
+			[", there", ""],
+			["!", "."]
+		}
+	} // Respoding Heirarchy Base
 	const func = function(input = "") {
 		// Token id 1 is a greeting. the AI must respond to it carefully.
 		// Token id 2 is a "how are you" question.
-		const regexes = [
-			{
-				regex: /h(?:ey(?:a)|i(?:ya?)|ello|owdy)(?:(,?)\s*there?)\s*(\w+)?/gi,
-				parse: function(match = "", name) {
+		const regexes = {
+				regex: /h(?:ey(?:a)|i(?:ya?)|ello|owdy)(?:(,?)\s*there?)\s*(\w+)?|(?:how're\s*you|how\s*(?:(?:was|is)\s*(?:your|(\w+)'s)\s*(?:day|week))|how's\s*(?:(?:your|(\w+)'s)\s*(?:day|week)))/gi,
+				parse: function(match = "", ...name) {
 					return {
-						token: 1,
-						name
+						token: (match !== "") + 1,
+						name: name.find(name => !!name)
 					}
 				}
-			},
-			{
-				regex: /(?:how're\s*you|how\s*(?:(?:was|is)\s*(?:your|(\w+)'s)\s*(?:day|week))|how's\s*(?:(?:your|(\w+)'s)\s*(?:day|week)))/gi,
-				parse: function(match = "", name) {
-					return {
-						token: 2,
-						name
-					}
-				}
-			}
 		]
-		let ai = "", maxTokens = new Array(500), i = 0; // 500 is the amount of tokens there should be.
-		for (const parser of regexes) {
-			
-			i++;
+		let ai = "", i = 0, matches = input.match(regexes.regex).map(match => regexes.parse(match, /^(?:\w+)|(\w+)/g.match(match))); // 500 is the amount of tokens there should be.
+		// AI responds
+		let decisions = {
+			shouldGreet: false
 		}
+		for (const parse of matches) {
+			switch (parse.token) {
+				case 1:
+					decisions.shouldGreet = i === 0
+					break
+				case 2:
+					decisions.shouldGreet = false
+					ai += reheirbase.hello.reduce((item, result) => result + ranItem(item), "")
+					break
+			}
+			i++
+		}
+		return ai.trim()
 	}
 	return func
 })
